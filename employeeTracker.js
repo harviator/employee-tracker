72f7462ab11b5b3f2d869c2ad15937158ee9ddfe
Harviator <78506767+harviator@users.noreply.github.com>
2021-06-21 08:56:52 -0400

    Added initalization function

--- employeeTracker.js
+++ employeeTracker.js
@@ -15,6 +15,28 @@ const connection = mysql.createConnection({
 
 // Initialize the application
 //Options for add, view, update, and delete
+const init = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Add (Departments, Roles, and Employees)", "View (Departments, Roles, Employees)", "Update (Employee Roles)", "Exit"],
+            name: "init",
+        }
+    ])
+        .then((response) => {
+
+            if (response.init == "Add (Departments, Roles, and Employees)") {
+                // Add function
+            } else if (response.init == "View (Departments, Roles, Employees)") {
+                // View function
+            } else if (response.init == "Update (Employee Roles)") {
+                // Update function
+            }  else if (response.init == "Exit") {
+                connection.end();
+            }
+        })
+}
 
 //Add
 //Department, role, or employee
