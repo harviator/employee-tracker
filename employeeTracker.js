87d6fa9f7af8342ac065705c229250183f5d5551
Harviator <78506767+harviator@users.noreply.github.com>
2021-06-21 07:40:14 -0400

    Added pseudocode to orgianize thoughts and tasks

--- employeeTracker.js
+++ employeeTracker.js
@@ -13,10 +13,24 @@ const connection = mysql.createConnection({
     database: 'employee_trackerDB',
 });
 
+// Initialize the application
+//Options for add, view, update, and delete
+
+//Add
+//Department, role, or employee
+
+//View
+//Department, role, or employee (if time view employees by manager and department by combined salary)
+
+//Update
+//Employee role (if time employee managers)
+
+//Delete (if time)
+//Department, role, or employee
 
 
 // Conntect to MySQL server and database
 connection.connect((err) => {
     if (err) throw err;
-    init()
+    init();
 })
